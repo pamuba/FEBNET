594eb5f2d89930300992908d8c227935c4cdaf5f
pamuba <me.in@live.in>
2024-03-30 02:52:26 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp
+{
+
+    class RefTest {
+
+        public void dis() {
+
+            unsafe
+            {
+                int i = 100;
+                int* j = &i;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
